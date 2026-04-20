5ede4f88b1e361c6eb70b7581dfb28fac2c9a6c0
coscreativez-main <coscreativez@gmail.com>
2026-04-20 18:42:54 +0530

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,6 +3,8 @@ const nav = document.querySelector(".nav");
 
 menuToggle.addEventListener("click", () => {
   nav.classList.toggle("active");
+    menuToggle.classList.toggle("active"); // <-- add this line
+
 });
 
 // Story Section
